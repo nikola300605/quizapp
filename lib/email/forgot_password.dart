046a5ceec1b74b9f4d693633b4afaaacbc9c0c12
nikola300605 <nikolacupic555@gmail.com>
2023-02-07 12:52:35 +0100

    forgot password

--- lib/email/forgot_password.dart
+++ lib/email/forgot_password.dart
@@ -1,6 +1,8 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:quizapp/shared/utils.dart';
 
 class ForgotPaswordScreen extends StatefulWidget {
   const ForgotPaswordScreen({super.key});
@@ -10,10 +12,74 @@ class ForgotPaswordScreen extends StatefulWidget {
 }
 
 class _ForgotPaswordScreenState extends State<ForgotPaswordScreen> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Recieve an email to \n reset your password.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: emailController,
+                cursorColor: Colors.white,
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(labelText: "Email"),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (email) =>
+                    email != null && !EmailValidator.validate(email)
+                        ? 'Enter a valid email'
+                        : null,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton.icon(
+                label: const Text("Reset password"),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                ),
+                icon: const Icon(FontAwesomeIcons.envelope),
+                onPressed: () => resetPassword(),
+              )
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Future<void> resetPassword() async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => Center(
+              child: CircularProgressIndicator(),
+            ));
+
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: emailController.text.trim());
+      Utils.showSnackBar('Password Reset Email Sent');
+      Navigator.of(context)
+          .pushNamedAndRemoveUntil('/authpage', (route) => false);
+    } on FirebaseAuthException catch (e) {
+      Utils.showSnackBar(e.message);
+      Navigator.of(context).pop();
+    }
   }
 }
